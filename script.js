50720e616d3d21ecd892716931abe95fc9bac03e
N-eeraj <neeraj7rajeena@gmail.com>
2022-05-30 00:03:23 +0530

    Added header section and other minor refactoring

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const navLinks = document.querySelector("nav > div");
-
-const toggleMenu = () => {
-    navLinks.classList.toggle("open");
-};
+const toggleMenu = () => document.querySelector("nav > div").classList.toggle("open");
 
 document.getElementById("menu").addEventListener("click", toggleMenu);
 document.querySelectorAll("nav a").forEach(link => link.addEventListener("click", toggleMenu));
