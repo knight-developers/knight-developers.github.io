9cbe297698fd7cce416eca5b5a7cd9e75347f792
N-eeraj <neeraj7rajeena@gmail.com>
2022-06-02 22:38:23 +0530

    Added dynamic services & fix background issue

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // initializations
 const navBar = document.querySelector("nav");
+const servicesContainer = document.querySelector("#services > div");
 
 // functions
 const toggleMenu = () => document.querySelector("nav > div").classList.toggle("open");
+const serviceTemplate = service => {
+    return `
+        <article>
+            <div>
+                <h3>${service.service}</h3>
+                <p>${service.description}</p>
+            </div>
+            <img src="${service.illustration}" alt="${service.service}">
+        </article>
+    `;
+};
 
 // event listener functions
 document.getElementById("menu").addEventListener("click", toggleMenu);
@@ -14,7 +26,7 @@ onload = () => {
     fetch('assets/data/services.json')
     .then(respnonse => respnonse.json())
     .then(data => {
-        console.log(data);
+        data.forEach(service => servicesContainer.innerHTML += serviceTemplate(service));
     })
     .catch(err => console.error(err))
     .finally(() => document.querySelector("body > div").style.display = "none");
