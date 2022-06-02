2a4d329de0f716523b3834598a90b019dde9c0df
N-eeraj <neeraj7rajeena@gmail.com>
2022-06-02 21:55:16 +0530

    Added loading screen, style for services section and fetched services

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// initializations
+const navBar = document.querySelector("nav");
+
+// functions
 const toggleMenu = () => document.querySelector("nav > div").classList.toggle("open");
 
+// event listener functions
 document.getElementById("menu").addEventListener("click", toggleMenu);
 document.querySelectorAll("nav a").forEach(link => link.addEventListener("click", toggleMenu));
+
+
+// window events
+onload = () => {
+    fetch('assets/data/services.json')
+    .then(respnonse => respnonse.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.error(err))
+    .finally(() => document.querySelector("body > div").style.display = "none");
+};
+
+onscroll = () => {
+    if (scrollY > innerHeight/4)
+        navBar.style.backgroundColor = "var(--primary)";
+    else
+        navBar.style.backgroundColor = "transparent";
+}
