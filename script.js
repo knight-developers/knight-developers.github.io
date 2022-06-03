a285363aef11a1cd91552b29caac1d1b86824f4e
N-eeraj <neeraj7rajeena@gmail.com>
2022-06-03 22:33:43 +0530

    Added footer section

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// initializations
+////////// Initializations //////////
+const fetchLinks = ['assets/data/services.json', 'assets/data/contacts.json']; // links to fetch data
+// DOM Objects
 const navBar = document.querySelector("nav");
 const servicesContainer = document.querySelector("#services > div");
+const contactsList = document.querySelector("footer ul");
 
-// functions
+
+////////// Functions //////////
 const toggleMenu = () => document.querySelector("nav > div").classList.toggle("open");
-const serviceTemplate = service => {
-    return `
+
+// This function returns the response of the fetch request to the url as a promise
+const createPromise = url => new Promise((resolve, reject) => 
+        fetch(url)
+        .then(respnonse => resolve(respnonse.json()))
+        .catch(err => reject(err))
+    );
+
+// HTML Template Generator Functions
+const serviceTemplate = service => `
         <article>
             <div>
                 <h3>${service.service}</h3>
@@ -14,19 +26,25 @@ const serviceTemplate = service => {
             <img src="${service.illustration}" alt="${service.service}">
         </article>
     `;
-};
 
-// event listener functions
+const contactTemplate = contact => `
+        <li>
+            <a href="${contact.link}" target="_blank"><i class="${contact.iconClasses}"></i></a>
+        </li>
+    `;
+
+
+////////// Event Listener Functions //////////
 document.getElementById("menu").addEventListener("click", toggleMenu);
 document.querySelectorAll("nav a").forEach(link => link.addEventListener("click", toggleMenu));
 
 
-// window events
+////////// Window Events //////////
 onload = () => {
-    fetch('assets/data/services.json')
-    .then(respnonse => respnonse.json())
-    .then(data => {
-        data.forEach(service => servicesContainer.innerHTML += serviceTemplate(service));
+    Promise.all(fetchLinks.map(link => createPromise(link)))
+    .then(([services, contacts]) => {
+        services.forEach(service => servicesContainer.innerHTML += serviceTemplate(service));
+        contacts.forEach(contact => contactsList.innerHTML += contactTemplate(contact));
     })
     .catch(err => console.error(err))
     .finally(() => document.querySelector("body > div").style.display = "none");
